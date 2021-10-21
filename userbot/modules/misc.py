b88c03bf0d5bd84c8ab6cddd75020a5119abe012
ashisharmy <77767274+ashisharmy@users.noreply.github.com>
2021-10-22 02:06:42 +0530

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -76,11 +76,11 @@ async def killdabot(event):
 async def reedme(e):
     await e.edit(
         "**Here's something for you to read:**\n"
-        "\n[Setup Guide - Basic](https://kenharris.xyz/userbot.html)"
-        "\n[Setup Guide - Google Drive](https://telegra.ph/How-To-Setup-Google-Drive-04-03)"
-        "\n[Setup Guide - LastFM Module](https://telegra.ph/How-to-set-up-LastFM-module-for-Paperplane-userbot-11-02)"
-        "\n[Video Tutorial - 576p](https://mega.nz/#!ErwCESbJ!1ZvYAKdTEfb6y1FnqqiLhHH9vZg4UB2QZNYL9fbQ9vs)"
-        "\n[Video Tutorial - 1080p](https://mega.nz/#!x3JVhYwR!u7Uj0nvD8_CyyARrdKrFqlZEBFTnSVEiqts36HBMr-o)"
+        "\n[Setup Guide - Basic](https://t.me/CheemsUserbot/98)"
+        "\n[Setup Guide - Google Drive](https://t.me/CheemsUserbot/98)"
+        "\n[Setup Guide - LastFM Module](https://t.me/CheemsUserbot/98)"
+        "\n[Video Tutorial - 576p](https://t.me/CheemsUserbot/127)"
+        "\n[Video Tutorial - 1080p](https://t.me/CheemsUserbot/127)"
     )
 
 
@@ -103,7 +103,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """For .repo command, just returns the repo URL."""
     await wannasee.edit(
-        "Click [here](https://github.com/KenHV/KensurBot) to open the bot's GitHub page."
+        "Click [here](https://github.com/ashisharmy/CheemsBot) to open the bot's GitHub page."
     )
 
 
