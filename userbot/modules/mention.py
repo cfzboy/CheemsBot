db89ed46f5582773018cfc352177abe874533e4c
ashisharmy <77767274+ashisharmy@users.noreply.github.com>
2021-10-22 02:09:40 +0530

    Update mention.py

--- userbot/modules/mention.py
+++ userbot/modules/mention.py
@@ -61,7 +61,7 @@ async def mention(event):
 CMD_HELP.update(
     {
         "mention": "Mention users with a custom name."
-        "\nUsage: `Hi @ender1324[bluid boi]`"
-        "\nResult: Hi [bluid boi](tg://resolve?domain=ender1324)"
+        "\nUsage: `Hi @God_loverr[piro]`"
+        "\nResult: Hi [piro](tg://resolve?domain=god_loverr)"
     }
 )
