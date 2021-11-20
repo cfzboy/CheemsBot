7baf8ec58b76a12b70d1263c72f58f58ea49eb63
ashisharmy <77767274+ashisharmy@users.noreply.github.com>
2021-11-20 17:59:19 +0530

    Create vctools.py

---
+++ userbot/modules/vctools.py
@@ -0,0 +1,119 @@
+# Copyright (C) 2021 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+#
+# Ported by @mrismanaziz
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
+#
+# Kalo mau ngecopas, jangan hapus credit ya goblok
+
+from telethon.tl.functions.channels import GetFullChannelRequest as getchat
+from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
+from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
+from telethon.tl.functions.phone import EditGroupCallTitleRequest as settitle
+from telethon.tl.functions.phone import GetGroupCallRequest as getvc
+from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
+
+from userbot import ALIVE_NAME
+from userbot import CMD_HELP
+from userbot.utils import edit_delete, edit_or_reply, man_cmd
+
+
+async def get_call(event):
+    mm = await event.client(getchat(event.chat_id))
+    xx = await event.client(getvc(mm.full_chat.call))
+    return xx.call
+
+
+def user_list(l, n):
+    for i in range(0, len(l), n):
+        yield l[i : i + n]
+
+
+@register(outgoing=True, pattern=r"^\.startvc$")
+async def start_voice(c):
+    chat = await c.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not admin and not creator:
+        await edit_delete(c, f"**Sorry {ALIVE_NAME} Not an Admin 👮**")
+        return
+    try:
+        await c.client(startvc(c.chat_id))
+        await edit_or_reply(c, "`Voice Chat Started...`")
+    except Exception as ex:
+        await edit_delete(c, f"**ERROR:** `{ex}`")
+
+
+@register(outgoing=True, pattern=r"^\.stopvc$")
+async def stop_voice(c):
+    chat = await c.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not admin and not creator:
+        await edit_delete(c, f"**Sorry {ALIVE_NAME} Not an Admin 👮**")
+        return
+    try:
+        await c.client(stopvc(await get_call(c)))
+        await edit_or_reply(c, "`Voice Chat Stopped...`")
+    except Exception as ex:
+        await edit_delete(c, f"**ERROR:** `{ex}`")
+
+
+@register(outgoing=True, pattern=r"^\.vcinvite$")
+async def _(c):
+    xxnx = await edit_or_reply(c, "`Inviting Members to Voice Chat...`")
+    users = []
+    z = 0
+    async for x in c.client.iter_participants(c.chat_id):
+        if not x.bot:
+            users.append(x.id)
+    botman = list(user_list(users, 6))
+    for p in botman:
+        try:
+            await c.client(invitetovc(call=await get_call(c), users=p))
+            z += 6
+        except BaseException:
+            pass
+    await xxnx.edit(f"`{z}` **Successfully People are invited to VC**")
+
+
+@register(outgoing=True, pattern=r"^\.vctitle$")
+async def change_title(e):
+    title = e.pattern_match.group(1)
+    chat = await e.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not title:
+        return await edit_delete(e, "**Please enter the title of the group voice chat**")
+
+    if not admin and not creator:
+        await edit_delete(e, f"**Sorry {ALIVE_NAME} Not an Admin 👮**")
+        return
+    try:
+        await e.client(settitle(call=await get_call(e), title=title.strip()))
+        await edit_or_reply(e, f"**Successfully Changed VC Title To** `{title}`")
+    except Exception as ex:
+        await edit_delete(e, f"**ERROR:** `{ex}`")
+
+
+CMD_HELP.update(
+    {
+        "vc": f"**Plugin : **`vc`\
+        \n\n  •  **Syntax :** `.startvc`\
+        \n  •  **Function : **To Start a voice chat group\
+        \n\n  •  **Syntax :** `.stopvc`\
+        \n  •  **Function : **To Stop group voice chat\
+        \n\n  •  **Syntax :** `.vctitle` <title vcg>\
+        \n  •  **Function : **To Change the title/title of the voice chat group\
+        \n\n  •  **Syntax :** `.vcinvite`\
+        \n  •  **Function : **Invite group members to voice chat group\
+    "
+    }
+)
